b27b57c9240c8086177927377723a7d336dd921d
xiaoyu <877469082@qq.com>
2019-12-30 18:56:50 +0800

    在idea中测试GitHub的使用

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Hello how2j!");
         System.out.println("新增加的内容");
         System.out.println("在GitHub中新增加的内容");
+        System.out.println("在idea中新增加的内容");
     }
 }
