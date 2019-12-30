03320e443a13649c1ce6e25ccdb36d8b0b02e843
yutongxue007 <39624896+yutongxue007@users.noreply.github.com>
2019-12-30 18:48:06 +0800

    Update Main.java
    在GitHub中新增加的内容

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello how2j!");
         System.out.println("新增加的内容");
+        System.out.println("在GitHub中新增加的内容");
     }
 }
